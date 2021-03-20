2f75659d5051d8868b3d53d5c5fb09b6c726dcec
Rasika Deodhar <rasikadeodhar95@gmail.com>
2021-03-20 23:57:43 +0000

    Reading File names and ids from folder

---
+++ code.py
@@ -0,0 +1,22 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SERVICE_ACCOUNT_FILE = 'keys.json'
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata']
+
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+service = build('drive', 'v3', credentials=creds)
+
+results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+items = results.get('files', [])
+
+if not items:
+    print('No files found.')
+else:
+    print('Files:')
+    for item in items:
+        print(u'{0} ({1})'.format(item['name'], item['id']))
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Budget Application Using Google API
+
+## Idea:
+
+Google sheets for monthly bugdet and annual budget will be entered in an automated way.
+
+## References :
+
+1. https://www.youtube.com/watch?v=4ssigWmExak&ab_channel=LearnGoogleSpreadsheets
+2. https://console.developers.google.com
+3. https://developers.google.com/sheets/api/quickstart/python​
+4. https://developers.google.com/identity/protocols/oauth2/service-account#python​
+5. https://www.python.org/​
+6. https://code.visualstudio.com/
+7. https://developers.google.com/sheets/api/reference/rest
+8. https://towardsdatascience.com/how-to-manage-files-in-google-drive-with-python-d26471d91ecd
